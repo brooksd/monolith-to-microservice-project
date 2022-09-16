8df5e6bec5f36098f70ccffb0f969f80dcb87432
Donald Brooks <brooksd079@gmail.com>
2022-09-17 01:37:46 +0300

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=brooks
+export POSTGRES_PASSWORD=Access123!
+export POSTGRES_HOST=udrg.cqy4ibx5yegk.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagramprojectaws
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
